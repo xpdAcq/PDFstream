00ff616688bf0b44ba27fd6a16bc7673d09a365c
st3107 <st3107@columbia.edu>
2020-08-11 16:16:58 -0400

    TST: visualization 100 coverage

--- tests/visualization/test_main.py
+++ tests/visualization/test_main.py
@@ -9,9 +9,11 @@ import pdfstream.visualization.main as vis
         (['Ni_gr', 'Ni_gr'], {'mode': 'line', 'legends': ['Ni0', 'Ni1'], 'label': 'gr'}),
         (['Ni_gr', 'Ni_gr'], {'mode': 'line', 'stack': False}),
         (['Ni_gr', 'Ni_gr'], {'mode': 'line', 'xy_kwargs': {'color': 'black'}, 'texts': ['Ni0', 'Ni1']}),
+        (['Ni_gr', 'Ni_gr'], {'mode': 'line', 'colors': ['r', 'g']}),
         (['Ni_fgr', 'Ni_fgr'], {'mode': 'fit', 'texts': ['Ni0', 'Ni1'], 'label': 'gr'}),
         (['Ni_fgr', 'Ni_fgr'], {'mode': 'fit', 'stack': False}),
         (['Ni_fgr', 'Ni_fgr'], {'mode': 'fit', 'xy_kwargs': {'color': 'black'}}),
+        (['Ni_fgr', 'Ni_fgr'], {'mode': 'fit', 'colors': ['r', 'g']})
     ]
 )
 def test_waterfall(db, keys, kwargs):
