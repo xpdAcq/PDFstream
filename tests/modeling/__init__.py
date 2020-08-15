1f2d335e616add7f6616cb8bcba251251be7910b
st3107 <st3107@columbia.edu>
2020-08-15 15:09:02 -0400

    MNT: add useful functions to the module level

---
+++ tests/modeling/__init__.py
@@ -0,0 +1,16 @@
+from pdfstream.modeling.main import F, multi_phase, optimize, view_fits, report
+from pdfstream.save import save
+from pyobjcryst import loadCrystal
+from diffpy.structure import loadStructure
+
+
+__all__ = [
+    "F",
+    "multi_phase",
+    "optimize",
+    "view_fits",
+    "report",
+    "save",
+    "loadCrystal",
+    "loadStructure"
+]
