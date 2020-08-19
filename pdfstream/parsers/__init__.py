052a69dd5cd29d05db1f1d441c2d0748a735df09
st3107 <st3107@columbia.edu>
2020-08-19 17:56:47 -0400

    DEV: add dict_to_array in __int__.py

--- pdfstream/parsers/__init__.py
+++ pdfstream/parsers/__init__.py
@@ -1,10 +1,11 @@
 from pdfstream.parsers.atoms import dict_to_atoms
-from pdfstream.parsers.fitrecipe import recipe_to_dict
 from pdfstream.parsers.ciffile import cif_to_dict
-
+from pdfstream.parsers.fitdata import dict_to_array
+from pdfstream.parsers.fitrecipe import recipe_to_dict
 
 __all__ = [
     "recipe_to_dict",
     "dict_to_atoms",
-    "cif_to_dict"
+    "cif_to_dict",
+    "dict_to_array"
 ]
