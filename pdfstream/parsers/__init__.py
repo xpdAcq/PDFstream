f50fe02274ababfec8c8af5e0a2cac9726eb6a71
st3107 <st3107@columbia.edu>
2020-08-19 11:11:48 -0400

    MNT: add cif_to_dict in __int__.py

--- pdfstream/parsers/__init__.py
+++ pdfstream/parsers/__init__.py
@@ -1,8 +1,10 @@
 from pdfstream.parsers.dict_to_atoms import dict_to_atoms
 from pdfstream.parsers.recipe_to_dict import recipe_to_dict
+from pdfstream.parsers.cif_to_dict import cif_to_dict
 
 
 __all__ = [
     "recipe_to_dict",
-    "dict_to_atoms"
+    "dict_to_atoms",
+    "cif_to_dict"
 ]
