487008ee1eb4e1b15b3580e53ee936cfdeceb494
st3107 <st3107@columbia.edu>
2020-08-22 12:30:37 -0400

    CI: install package itself

---
+++ release.py
@@ -0,0 +1,17 @@
+"""Used in release procedure."""
+
+
+def conda_recipe():
+    """Make conda recipe."""
+    return
+
+
+def conda_build_config():
+    """Make the dictionary of conda build configuration."""
+
+    return
+
+
+def conda_meta():
+    """Make the dictionary of conda meta information."""
+    return
