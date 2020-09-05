148e2779b95223309e91e21819b5b450380d710e
st3107 <st3107@columbia.edu>
2020-09-05 10:35:33 -0400

    MNT: rename loadData to load_parser; move to io.py

--- pdfstream/parsers/__init__.py
+++ pdfstream/parsers/__init__.py
@@ -3,10 +3,8 @@ from pdfstream.parsers.ciffile import cif_to_dict
 from pdfstream.parsers.fitdata import dict_to_array, dicts_to_array
 from pdfstream.parsers.fitrecipe import recipe_to_dict
 
-__all__ = [
-    "recipe_to_dict",
-    "dict_to_atoms",
-    "cif_to_dict",
-    "dict_to_array",
-    "dicts_to_array"
-]
+dict_to_atoms = dict_to_atoms
+cif_to_dict = cif_to_dict
+dict_to_array = dict_to_array
+dicts_to_array = dicts_to_array
+recipe_to_dict = recipe_to_dict
