420f8bf9734e5b410dd12de9acbc410eaec8e8ad
st3107 <st3107@columbia.edu>
2020-09-04 21:05:34 -0400

    DOC: add doc for adding.py

--- pdfstream/modeling/adding.py
+++ pdfstream/modeling/adding.py
@@ -17,6 +17,54 @@ def initialize(
         xyz: tp.Union[str, None] = "s",
         params: tp.Union[None, str, tp.List[str]] = "a"
 ) -> None:
+    """Initialize a single-contribution recipe with the variables.
+
+    The variables will be constrained, created and added according to the mode indicated by the arguments. If
+    an argument is None, nothing will be done. It is assumed that the recipe only has one contribution and
+    all variables in the generators in that contribution will be initialized in the same way. To constrain
+    specific contribution and generator, please use `~pdfstream.modeling.adding.add_con_vars` and
+    `~pdfstream.modeling.adding.add_gen_vars`.
+
+    Parameters
+    ----------
+    recipe :
+        A recipe with a single contribution and generators.
+
+    scale :
+        Whether to add the scale of the generator. Default True.
+
+    delta :
+        If "1", add delta1 parameter.
+        If "2", add delta2 parameter.
+        If None, do nothing.
+
+    lat :
+        If "s", constrain the lattice parameters by space group and add the independent variables.
+        If "a", add all the lattice paramters.
+        If None, do nothing.
+        Default "s".
+
+    adp :
+        If "a", add all the Biso parameter.
+        If the structure is `~pyobjcryst.crystal.Crystal`, this step means add all the Biso of unique atoms.
+        If "e", constrain the Biso by elements, add the independent Biso.
+        If "s", constrain the B-tensor by space group, add all the independent diagonal terms like B11, B22, B33
+        or Biso.
+        If None, do nothing.
+        Default "a".
+
+    xyz :
+        If "s", constrain the coordinates of atoms by space group and add independent coordinates.
+        If "a", add all coordinates of atoms.
+        If None, do nothing.
+        Default "s".
+
+    params :
+        If "a", add all the parameters in the equation and characteristic functions like "psize".
+        If list of str, add all the parameters whose names are in the list.
+        If None, do nothing.
+        Default "a".
+    """
     add_con_vars(recipe, params=params)
     add_gen_vars(recipe, scale=scale, delta=delta, lat=lat, adp=adp, xyz=xyz)
     return
@@ -27,6 +75,11 @@ def add_con_vars(
         name: tp.Union[str, None] = None,
         params: tp.Union[None, str, tp.List[str]] = "a"
 ):
+    """Add variables at contribution level.
+
+    The name is the name of the contribution. If None, all the contribution will be searched and added.
+
+    """
     if not name:
         for con_name in recipe.contributions.keys():
             add_con_vars(recipe, con_name, params)
@@ -37,6 +90,7 @@ def add_con_vars(
 
 
 def add_params(recipe: MyRecipe, con: MyContribution, params: tp.Union[None, str, tp.List[str]]) -> None:
+    """Add contribution-level parameters in the contribution."""
     if not params:
         return
     args = {
@@ -64,6 +118,10 @@ def add_gen_vars(
         adp: tp.Union[str, None] = "a",
         xyz: tp.Union[str, None] = "s",
 ) -> None:
+    """Add parameters at generator level.
+
+    The names are (contribution name, generator name). If None, all generators will be constrained and added.
+    """
     if not names:
         for con_name, con in recipe.contributions.items():
             for gen_name in con.generators.keys():
@@ -81,6 +139,7 @@ def add_gen_vars(
 
 
 def add_scale(recipe: MyRecipe, gen: G, scale: bool = True) -> None:
+    """Add the scale of the generator."""
     if not scale:
         return
     recipe.addVar(
@@ -95,6 +154,7 @@ def add_scale(recipe: MyRecipe, gen: G, scale: bool = True) -> None:
 
 
 def add_delta(recipe: MyRecipe, gen: G, delta: tp.Union[str, None]) -> None:
+    """Add the delta parameter of the generator."""
     if not delta:
         return
     if delta == "1":
@@ -115,6 +175,7 @@ def add_delta(recipe: MyRecipe, gen: G, delta: tp.Union[str, None]) -> None:
 
 
 def add_lat(recipe: MyRecipe, gen: G, lat: tp.Union[str, None]) -> None:
+    """Add the lattice parameters of the phase."""
     if not lat:
         return
     if lat == "s":
@@ -134,7 +195,11 @@ def add_lat(recipe: MyRecipe, gen: G, lat: tp.Union[str, None]) -> None:
     return
 
 
-def add_adp(recipe: MyRecipe, gen: G, adp: tp.Union[str, None]) -> None:
+def add_adp(
+        recipe: MyRecipe, gen: G, adp: tp.Union[str, None],
+        symbols: tp.tuple[str] = ("Biso", "B11", "B22", "B33")
+) -> None:
+    """Add the atomic displacement parameter of the phase."""
     if not adp:
         return
     atoms = gen.phase.getScatterers()
@@ -155,7 +220,10 @@ def add_adp(recipe: MyRecipe, gen: G, adp: tp.Union[str, None]) -> None:
         names = ["Biso_{}".format(bleach(atom.name)) for atom in atoms]
     elif adp == "s":
         pars = gen.phase.sgpars.adppars
-        names = [rename_by_atom(par.name, atoms) for par in pars]
+        names = [
+            rename_by_atom(par.name, atoms) for par in pars
+            if par.name.split("_")[0] in symbols
+        ]
     else:
         raise ValueError("Unknown adp: {}. Allowed: element, sg, all.".format(adp))
     for par, name in zip(pars, names):
@@ -171,6 +239,7 @@ def add_adp(recipe: MyRecipe, gen: G, adp: tp.Union[str, None]) -> None:
 
 
 def add_xyz(recipe: MyRecipe, gen: G, xyz: tp.Union[str, None]) -> None:
+    """Add the coordinates of the atoms in the phase."""
     if not xyz:
         return
     atoms = gen.phase.getScatterers()
@@ -198,6 +267,8 @@ def add_xyz(recipe: MyRecipe, gen: G, xyz: tp.Union[str, None]) -> None:
 
 
 def rename_by_atom(name: str, atoms: list) -> str:
+    """Rename of the name of a parameter by replacing the index of the atom in the name by the label of
+    the atom. Used for the space group constrained parameters."""
     parts = name.split("_")
     if len(parts) > 1 and parts[1].isdigit() and -1 < int(parts[1]) < len(atoms):
         parts[1] = atoms[int(parts[1])].name
@@ -205,4 +276,5 @@ def rename_by_atom(name: str, atoms: list) -> str:
 
 
 def bleach(s: str):
+    """Strip all the characters except the number and letters."""
     return ''.join((c for c in s if c.isalnum()))
