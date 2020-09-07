ee7de36f14e82189dbf9b935b7d9942b71842e29
st3107 <st3107@columbia.edu>
2020-09-07 14:39:33 -0400

    BUG: debug fitted_curves

--- pdfstream/visualization/docs.py
+++ pdfstream/visualization/docs.py
@@ -44,7 +44,7 @@ def fitted_curves(docs: tp.Iterable[dict], text_keys: tuple = None, keys: tuple 
     """
     data = parsers.dicts_to_array(docs, keys=keys, data_keys=data_keys)
     rws = parsers.dicts_to_array(docs, keys=keys, data_keys=(rw_key,))
-    rws = np.squeeze(rws)
+    rws = np.squeeze(rws) if rws.ndim > 1 else rws
     texts = [rw_template.format(rw) for rw in rws]
     if text_keys:
         raw_texts = parsers.dicts_to_array(docs, keys=text_keys[:-1], data_keys=(text_keys[-1],))
