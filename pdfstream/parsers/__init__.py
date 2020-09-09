c71285c4bc270f6dc483e0edafdac971fc556c72
st3107 <st3107@columbia.edu>
2020-09-09 16:28:38 -0400

    MNT: add to module level

--- pdfstream/parsers/__init__.py
+++ pdfstream/parsers/__init__.py
@@ -1,10 +1,13 @@
-from pdfstream.parsers.atoms import dict_to_atoms
+from pdfstream.parsers.atoms import dict_to_atoms, dict_to_atoms2
 from pdfstream.parsers.ciffile import cif_to_dict
 from pdfstream.parsers.fitdata import dict_to_array, dicts_to_array
 from pdfstream.parsers.fitrecipe import recipe_to_dict
+from pdfstream.parsers.fitrecipe import recipe_to_dict2
 
 dict_to_atoms = dict_to_atoms
 cif_to_dict = cif_to_dict
 dict_to_array = dict_to_array
 dicts_to_array = dicts_to_array
 recipe_to_dict = recipe_to_dict
+recipe_to_dict2 = recipe_to_dict2
+dict_to_atoms2 = dict_to_atoms2
