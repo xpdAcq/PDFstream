94ddcf1d8e37f3e3c5707c3f2a7f30e1c244f845
st3107 <st3107@columbia.edu>
2020-09-09 20:45:02 -0400

    MNT: add new funcs to module

--- pdfstream/parsers/__init__.py
+++ pdfstream/parsers/__init__.py
@@ -1,8 +1,9 @@
-from pdfstream.parsers.atoms import dict_to_atoms, dict_to_atoms2
-from pdfstream.parsers.ciffile import cif_to_dict
-from pdfstream.parsers.fitdata import dict_to_array, dicts_to_array
-from pdfstream.parsers.fitrecipe import recipe_to_dict
-from pdfstream.parsers.fitrecipe import recipe_to_dict2
+from .atoms import dict_to_atoms, dict_to_atoms2
+from .ciffile import cif_to_dict
+from .fitdata import dict_to_array, dicts_to_array
+from .fitrecipe import recipe_to_dict
+from .fitrecipe import recipe_to_dict2
+from .fitresult import rename_rule, to_latex, to_xarray
 
 dict_to_atoms = dict_to_atoms
 cif_to_dict = cif_to_dict
@@ -11,3 +12,6 @@ dicts_to_array = dicts_to_array
 recipe_to_dict = recipe_to_dict
 recipe_to_dict2 = recipe_to_dict2
 dict_to_atoms2 = dict_to_atoms2
+rename_rule = rename_rule
+to_latex = to_latex
+to_xarray = to_xarray
