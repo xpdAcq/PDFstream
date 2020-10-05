2f01fa06fa3288ea81646b8b2835475963735ff2
st3107 <st3107@columbia.edu>
2020-10-05 18:30:01 -0400

    TST: add test for iter_dict

---
+++ pdfstream/utils/dct_ops.py
@@ -0,0 +1,47 @@
+"""The tools to read, write, audit, transform, wash the data."""
+from typing import Any
+from typing import Callable
+from typing import Generator
+
+
+def paths(dct: Any, path=()) -> Generator:
+    """Yield paths to the leafs of a nested dictionary."""
+    if isinstance(dct, dict):
+        for key, value in dct.items():
+            yield from paths(value, path + (key,))
+    elif isinstance(dct, (tuple, list)):
+        for ind, elem in enumerate(dct):
+            yield from paths(elem, path + (ind,))
+    else:
+        yield path + (dct,)
+
+
+def iter_dct(dct: dict, operation: Callable):
+    """Recursively iterate the dictionary and operate on the leaf nodes. Return the processed dictionary."""
+    dct2 = dict()
+    for key, value in dct.items():
+        if isinstance(value, dict):
+            dct2[key] = iter_dct(value, operation)
+        else:
+            dct2[key] = operation(value)
+    return dct2
+
+
+def to_dict(obj: object, exclude: Callable[[str], bool] = None) -> dict:
+    """Convert an object to a dictionary by keeping import attribute information."""
+
+    def pdfconfig_exclude(attr: str) -> bool:
+        """Whether to exclude an attribute in pdfconfig."""
+        if attr.startswith("_"):
+            return True
+        if attr in ("inputfiles", "output", "interact"):
+            return True
+        return False
+
+    dct = {}
+    if exclude is None:
+        exclude = pdfconfig_exclude
+    for attr_key, attr_val in obj.__dict__.items():
+        if not exclude(attr_key):
+            dct[attr_key] = attr_val
+    return dct
---
+++ tests/utils/test_dct_ops.py
@@ -0,0 +1,40 @@
+import pytest
+
+import pdfstream.utils.dct_ops as dct_ops
+
+
+def test_paths():
+    dct = {
+        "k1": {
+            "k11": "v11",
+            "k12": {
+                "k121": "v121"
+            }
+        },
+        "k2": ["v21", "v22"]
+    }
+    assert list(dct_ops.paths(dct)) == [
+        ("k1", "k11", "v11"),
+        ("k1", "k12", "k121", "v121"),
+        ("k2", 0, "v21"),
+        ("k2", 1, "v22")
+    ]
+
+
+def test_to_dict(db):
+    dct_ops.to_dict(db["Ni_config"])
+
+
+@pytest.mark.parametrize(
+    "dct, op, expect",
+    [
+        (
+            {"k0": {"k1": 0, "k2": 1}},
+            lambda x: x + 1,
+            {"k0": {"k1": 1, "k2": 2}}
+        )
+    ]
+)
+def test_iter_dct(dct, op, expect):
+    dct1 = dct_ops.iter_dct(dct, op)
+    assert dct1 == expect
