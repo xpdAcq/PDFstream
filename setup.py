e814c254737127e3403e9ef4909b37aff36141fc
st3107 <st3107@columbia.edu>
2020-10-08 17:03:45 -0400

    bumped version to 0.2.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
 
 setup(
     name='pdfstream',
-    version='0.2.1',
+    version='0.2.2',
     description="The data analysis toolbox for the study on pair distribution function (PDF).",
     long_description=readme,
     long_description_content_type='text/markdown',
