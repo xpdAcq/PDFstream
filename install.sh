4f12545043c7d004bca5c34cca6cbc92ebc17c7b
st3107 <st3107@columbia.edu>
2022-02-02 14:49:39 -0500

    BLD: add --yes

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 set -e
 read -p "Please give a name for the conda environment: " env
-conda create -n $env
-conda install -n $env -c conda-forge pdfstream
+conda create -n $env --yes
+conda install -n $env -c conda-forge pdfstream --yes
 echo "Package has been successfully installed."
 echo "Please run the following command to activate the environment."
 echo ""
