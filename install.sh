07537b2f1ed8ca5b2149fd6b6bf2c4e9b03ab468
st3107 <st3107@columbia.edu>
2022-04-13 13:29:02 -0400

    MNT: update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -e
-read -p "Please give a name for the conda environment: " env
-conda create -n $env --yes
-conda install -n $env -c conda-forge --file requirements/build.txt \
+read -r -p "Please give a name for the conda environment: " env
+conda create -n "$env" --yes
+conda install -n "$env" -c conda-forge --file requirements/build.txt \
 --file requirements/run.txt \
 --file requirements/test.txt \
 --file requirements/docs.txt \
 --yes
-conda run --live-stream --no-capture-output -n $env pip install -r requirements/pip.txt
-conda run --live-stream --no-capture-output -n $env pip install -e .
+conda run --live-stream --no-capture-output -n "$env" pip install -r requirements/pip.txt
+conda run --live-stream --no-capture-output -n "$env" pip install -e .
 echo "Package has been successfully installed."
 echo "Please run the following command to activate the environment."
 echo ""
