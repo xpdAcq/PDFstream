8ffc61ac32053b75eb25906328210ba11aa222b2
st3107 <st3107@columbia.edu>
2022-04-21 17:03:33 -0400

    DEV: add yaml serializer

---
+++ pdfstream/callbacks/yamlserializer.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import pdfstream.io as io
+import yaml
+from bluesky.callbacks.core import CallbackBase
+
+
+class YamlSerializer(CallbackBase):
+    """Export the start document in yaml file."""
+
+    def __init__(self, directory: str) -> None:
+        super().__init__()
+        self._directory = Path(directory)
+
+    def _get_filepath(self, doc: dict) -> Path:
+        return self._directory.joinpath(doc["filename"]).with_suffix(".yml")
+
+    def start(self, doc):
+        file_path = self._get_filepath(doc)
+        with file_path.open("w") as f:
+            yaml.dump(doc, f)
+        io.server_message("Save start document of '{}'.".format(doc["uid"]))
+        return doc
