cd7b44222bb13565c76fa874d5030d3f3a1d1771
st3107 <st3107@columbia.edu>
2022-04-21 17:02:59 -0400

    DEV: add TIffSerializer

---
+++ pdfstream/callbacks/tiffserilaizer.py
@@ -0,0 +1,41 @@
+import typing as T
+from pathlib import Path
+
+import numpy as np
+from bluesky.callbacks.core import CallbackBase
+from tifffile import TiffWriter
+
+
+class TiffSerializer(CallbackBase):
+
+    def __init__(self, fields: T.List[str], directory: str, dtype: str = "uint32", stream_name: str = "primary") -> None:
+        super().__init__()
+        self._fields = fields
+        self._dtype = dtype
+        self._stream_name = stream_name
+        self._directory = Path(directory)
+        self._descriptor = ""
+
+    def _get_filepath(self, filename: str, field: str) -> Path:
+        f = filename + "_" + field
+        filepath = self._directory.joinpath(f).with_suffix(".tiff")
+        return filepath
+
+    def _export(self, doc: dict, field: str) -> None:
+        image: np.ndarray = doc["data"][field].astype(self._dtype)
+        filepath = self._get_filepath(doc["data"]["filename"], field)
+        with TiffWriter(str(filepath)) as tf:
+            tf.save(image)
+        return
+
+    def descriptor(self, doc):
+        if doc["name"] == self._stream_name:
+            self._descriptor = doc['uid']
+        return doc
+
+    def event(self, doc):
+        if doc["descriptor"] == self._descriptor:
+            for field in self._fields:
+                if field in doc["data"]:
+                    self._export(doc, field)
+        return doc
