b38d6e468ac77fd3d3bcc6e7c889cd474b26647d
st3107 <st3107@columbia.edu>
2022-05-02 16:04:35 -0400

    TST: debug import

--- tests/test_servers.py
+++ tests/test_servers.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 from pdfstream.callbacks.config import Config
-from pdfstream.main import _make_server
 from pkg_resources import resource_filename
 
 CONFIG_FILE = Path(resource_filename("pdfstream", "data/config_files/simulation.ini"))
