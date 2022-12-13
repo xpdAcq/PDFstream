6ccc861f5b8bb82588756a19be28b17412d8c954
st3107 <st3107@columbia.edu>
2022-12-13 13:15:10 -0500

    bumped version to 0.6.2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open(
 
 setup(
     name="pdfstream",
-    version="0.6.1",
+    version='0.6.2',
     description="The configs analysis toolbox for the study on pair distribution function (PDF).",
     install_requires=requirements,
     long_description=readme,
